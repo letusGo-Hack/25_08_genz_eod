a07b3b9811acaa8ea8489feb8c23c544fbc30197
myungsun <audtjs9987@naver.com>
2025-07-19 09:43:17 +0900

    Initial Commit

---
+++ ClockOutFairy/ClockOutFairyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockOutFairyApp.swift
+//  ClockOutFairy
+//
+//  Created by myungsun on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockOutFairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
