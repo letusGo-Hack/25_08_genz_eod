da333ce395813fb559dda7b59d1e61e1de6ab8f9
jhkim <faithinker12@gmail.com>
2025-07-19 10:58:42 +0900

    Widget 기본 파일 생성

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by jhkim on 2025.07.19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
